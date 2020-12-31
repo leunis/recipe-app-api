61262302804af33f4ec9022f4a4957d3f9b6c270
Martijn Dekker <martijn@maf-internet.nl>
2020-12-31 11:46:07 +0100

    uthenticate users

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
